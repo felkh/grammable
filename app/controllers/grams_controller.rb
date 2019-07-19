682804269db4da029ca40b9cb1d6ab1026125923
felkh <felisa.kh@hotmail.com>
2019-07-19 14:43:18 +0000

    styled

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -26,6 +26,7 @@ class GramsController < ApplicationController
   end
 
   def index
+    @grams = Gram.all
   end
 
   def show
@@ -51,10 +52,6 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
-  end
-
-  def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize} :(", status: status
+    params.require(:gram).permit(:message, :picture)
   end
 end
